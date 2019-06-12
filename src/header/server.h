145d2b8c5e34d125d810ff5dbc45123aeb8dc70a
WizardLemon <nemanjalekic.ns@gmail.com>
2019-06-13 00:06:00 +0200

    packet handler WIP

--- src/header/server.h
+++ src/header/server.h
@@ -2,4 +2,6 @@
 #define SERVER_H
 #include <pcap.h>
 #include "utilities.h"
+
+void packet handler(unsigned char *interface, const struct pcap_pkthdr* packet_header, const unsigned char* packet_data);
 #endif // SERVER_H
